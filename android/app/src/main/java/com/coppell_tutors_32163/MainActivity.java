dda781a8ab4258fad4a6fac77b0146d7eb925f40
Crowdbotics <team@crowdbotics.com>
2021-11-26 18:24:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/coppell_tutors_32163/MainActivity.java
@@ -0,0 +1,15 @@
+package com.coppell_tutors_32163;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "coppell_tutors_32163";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "coppell_tutors_32163",
+  url: "https://coppell_tutors_32163.botics.co",
+  api: "https://coppell_tutors_32163.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
